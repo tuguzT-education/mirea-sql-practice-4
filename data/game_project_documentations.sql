c9f7264e5ce589e78b7b4dde7d314659bc8bb45d
tuguzT <timurka.tugushev@gmail.com>
2021-10-11 20:35:09 +0300

    Added info about some game projects

---
+++ data/game_project_documentations.sql
@@ -0,0 +1,15 @@
+INSERT INTO game_project_documentation (
+    game_project_business_plan,
+    game_project_design_document,
+    game_project_vision
+)
+VALUES (
+        'Hello World',
+        'Empty right now',
+        'No vision right now'
+       ),
+       (
+        'Just an example',
+        'Just an example',
+        'Just an example'
+       );
---
+++ data/game_projects.sql
@@ -0,0 +1,28 @@
+INSERT INTO game_project (
+    game_project_name,
+    game_project_description,
+    game_project_documentation_id
+)
+VALUES (
+        'Tetris',
+        'In Tetris, players complete lines by moving differently shaped pieces, '
+            'which descend onto the playing field. The completed lines disappear and grant the player points, '
+            'and the player can proceed to fill the vacated spaces. The game ends when the playing field is filled. '
+            'The longer the player can delay this inevitable outcome, the higher their score will be. '
+            'In multiplayer games, the players must last longer than their opponents, and in certain versions, '
+            'players can inflict penalties on opponents by completing a significant number of lines. '
+            'Some adaptations have provided variations to the game\'s theme, such as three-dimensional displays or '
+            'a system for reserving pieces.',
+        2
+       ),
+       (
+        'Minecraft',
+        'In Minecraft, players explore a blocky, procedurally-generated 3D world with virtually infinite terrain, '
+            'and may discover and extract raw materials, craft tools and items, and build structures or earthworks. '
+            'Depending on game mode, players can fight computer-controlled mobs, as well as cooperate with or '
+            'compete against other players in the same world. Game modes include a survival mode, '
+            'in which players must acquire resources to build the world and maintain health, '
+            'and a creative mode, where players have unlimited resources and access to flight. '
+            'Players can modify the game to create new gameplay mechanics, items, and assets.',
+        1
+       );
