a5c51848dcdc17bf10dea86e746097e7a35911b9
tuguzT <timurka.tugushev@gmail.com>
2021-10-11 21:07:52 +0300

    Added game project versions

---
+++ data/game_project_versions.sql
@@ -0,0 +1,26 @@
+INSERT INTO game_project_version (
+    game_project_version_hash,
+    game_project_version_major,
+    game_project_version_minor,
+    game_project_version_patch,
+    game_project_version_metadata,
+    game_project_id
+)
+VALUES (
+        'o9345yj53h4dnnf9t89',
+        1, 0, 0,
+        '',
+        1
+       ),
+       (
+        'e94bvytnemrhtc04h8v37t',
+        1, 18, 0,
+        '-21w40a',
+        2
+       ),
+       (
+        'hgfibsdjfgh9o74y348',
+        1, 16, 5,
+        '-rc1',
+        2
+       );
