0895b2d6f5e7813b206c654328134a52d514d53b
tuguzT <timurka.tugushev@gmail.com>
2021-10-11 21:03:00 +0300

    Added 2 game assets

---
+++ data/game_assets.sql
@@ -0,0 +1,21 @@
+INSERT INTO game_asset (
+    game_asset_name,
+    game_asset_type_id,
+    game_asset_description,
+    game_asset_data,
+    game_project_id
+)
+VALUES (
+        'Box',
+        2,
+        'One part of the blocky Tetris game',
+        'idk really',
+        1
+       ),
+       (
+        'creeper',
+        10,
+        '3D model of the deadliest destroyer of our buildings',
+        'BOOM!',
+        2
+       );
